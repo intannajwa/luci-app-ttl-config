9276277d27b0a767f117ff9a9910d8377983f4e5
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-30 00:32:29 +0800

    Update ttlconfig.lua

--- src/model/ttlconfig.lua
+++ src/model/ttlconfig.lua
@@ -1,32 +1,30 @@
-local fs = require("nixio.fs")
-local uci = require("uci").cursor()
+local m, s, o
+m = Map("ttl", translate("Antitethering Config"))
 
-module("luci.model.cbi.ttlconfig", package.seeall)
+s = m:section(TypedSection, "ttl", translate("TTL antitether"))
+s.anonymous = true
+s.addremove = true
 
--- Function to retrieve the current TTL value from the nftables configuration
-function get_current_ttl()
-    local nft_config = "/etc/nftables.d/10-custom-filter-chains.nft"
-    local command = "grep -oP 'ttl set \K[0-9]+' " .. nft_config
-    local ttl_value = io.popen(command):read("*a")
-    return ttl_value:match("%d+")
-end
+o = s:option(Value, "iface", translate("Set interface"))
 
--- Function to set TTL value
-function set_ttl_value(ttl_value)
-    local nft_config = "/etc/nftables.d/10-custom-filter-chains.nft"
-    local chains = {
-        "mangle_prerouting_ttl64",
-        "mangle_postrouting_ttl64",
-        "mangle_prerouting_hoplimit64",
-        "mangle_postrouting_hoplimit64"
-    }
+o = s:option(Flag, "advanced", translate("Advanced Option"))
+o.default = '0'
+o.rmempty = false
 
-    -- Loop through all the chains and update the TTL value
-    for _, chain in ipairs(chains) do
-        local command = "sed -i 's/^\(.*chain " .. chain .. ".*ttl set \)[0-9]\+/\1" .. ttl_value .. "/' " .. nft_config
-        os.execute(command)  -- Update the TTL value for the chain
+o = s:option(ListValue, "inet", translate("Inet Family"))
+o:value("ipv4", "IPv4")
+o:value("ipv6", "IPv6")
+o:value("ipv4v6", translate("Both"))
+o.rmempty = true
+o.editable = true
+o:depends("advanced", "1")
 
-        -- Apply the new nftables configuration
-        os.execute("nft -f " .. nft_config)
-    end
-end
+o = s:option(Value, "ttl", translate("TTL Value"))
+o:value("64", "64")
+o:value("128", "128")
+o.default = "64"
+o.rmempty = true
+o.editable = true
+o:depends("advanced", "1")
+
+return m
