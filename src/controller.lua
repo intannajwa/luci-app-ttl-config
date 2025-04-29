77056d4f7b2b623261dda70250e3d768d14530de
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-29 22:03:01 +0800

    Update controller.lua

--- src/controller.lua
+++ src/controller.lua
@@ -1,5 +1,18 @@
 module("luci.controller.ttlconfig", package.seeall)
 
+local ttlconfig = require("luci.model.cbi.ttlconfig")
+
 function index()
-    entry({"admin", "network", "ttl-config"}, cbi("ttl-config/ttlconfig"), _("TTL Config"), 60)
+    entry({"admin", "modem", "ttl-config"}, cbi("ttl-config/ttlconfig"), _("TTL Config"), 60)
+
+    -- Handle form submission
+    if luci.http.formvalue("submit") then
+        local ttl_value = luci.http.formvalue("ttl_value")
+        local chain = luci.http.formvalue("chain")
+        if ttl_value and chain then
+            ttlconfig.set_ttl_value(chain, ttl_value)  -- Apply the new TTL value
+            luci.sys.call("uci commit")  -- Commit changes if any
+            luci.http.redirect(luci.dispatcher.build_url("admin/network/ttl-config"))
+        end
+    end
 end
